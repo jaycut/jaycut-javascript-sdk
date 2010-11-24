8668efcd7f9fe1d052fce821518c8299672213aa
Jerker Karlsson <jerker@jaycut.com>
2010-11-24 17:54:20 +0100

    JC.init now accept options

--- js-sdk.js
+++ js-sdk.js
@@ -16,7 +16,38 @@ var _jaycut = {
     __event_handlers: {},
     __loaderUri: null,
     __appUri:  null,
-    __chainUri: null,
+    __options: {
+	'version': 2,
+	'applet': 'login',
+	'chain': 'mixer',
+	'chain_params': {},
+	'embed_target': 'jaycut-editor',
+	'embed_width': '100%',
+	'embed_height': '100%'
+    },
+    __flashvars: {},
+    __flashparams: {
+	wmode: 'window',
+	allowScriptAccess: 'always',
+	allowFullScreen: 'true',
+	bgcolor: '#000000',
+    },
+    __flashoptions: {
+	'noHelp': true,
+	'noUpload': true,
+	'noPublish': true,
+	'noFullscreen': true,
+	'noOpenSave': true,
+	'noWebcam': true,
+	'noMicrophone': true,
+	'mix': true,
+	'remix': true,
+	'redirectOnPublish': true,
+	'redirectWindow': true,
+	'requireMixName': true,
+	'locale': true,
+	'helpUri': true
+    },
 
     subscribe: function(event_name, func) {
 	this.__event_handlers[event_name] = func;
@@ -36,37 +67,45 @@ var _jaycut = {
     overrideApplicationUri: function(uri) {
 	this.__appUri = uri;
     },
-    overrideChainUri: function(uri) {
-	this.__chainUri = uri;
-    },
-    init: function(uri_authority, login_uri) {
+    init: function(options) {
+	if (options != null) {
+	    for (var key in options) {
+		if (key == 'flashparams') {
+		    for (var k in options['flashparams']) {
+			this.__flashparams[k] = options['flashparams'][k]
+		    }
+		} else if (this.__flashoptions[key] == true) {
+		    this.__flashvars[key] = options[key];
+		} else {
+		    this.__options[key] = options[key]
+		}
+	    }
+	}
 
-	if (this.__loaderUri == null)
-	    this.__loaderUri = 'http://' + uri_authority + '/assets/flash/ApplicationLoader.swf'
+	if (this.__loaderUri == null) {
+	    this.__loaderUri = 'http://' + this.__options['uri_authority'] + '/assets/flash/ApplicationLoader.swf'
+	}
 
-	if (this.__appUri == null)
-	    this.__appUri = 'http://' + uri_authority + '/applets/login.xml?chain=mixer';
+	if (this.__appUri == null) {
+	    this.__appUri = 'http://' + this.__options['uri_authority'];
+	    this.__appUri += '/applets/' + this.__options['applet'] + '.xml';
+	    this.__appUri += '?chain=' + this.__options['chain'];
+	    this.__appUri += '&version=' + this.__options['version'];
+	    this.__appUri += build_chain_params(this.__options['chain_params']);
+	}
 
-	var flashvars = {};
-	flashvars.applicationUri = encodeURIComponent(this.__appUri);
-	if (login_uri == null)
-	    // Why does this work?
-	    flashvars.loginUri = encodeURIComponent(this.__loaderUri);
-	else
-	    flashvars.loginUri = encodeURIComponent(login_uri);
-	if (this.__chainUri != null)
-	    flashvars.chainUri = this.__chainUri;
+	this.__flashvars.applicationUri = encodeURIComponent(this.__appUri);
 
-	var params = {};
-	params.wmode = 'window';
-	params.allowScriptAccess = 'always';
-	params.allowFullScreen = 'true';
-	params.bgcolor = '#000000';
+	if (this.__options['login_uri'] != null) {
+	    this.__flashvars.loginUri = encodeURIComponent(this.__options['login_uri']);
+	}
 
 	ensureSWFObjectScriptIsLoaded();
 
 	__run_when_swfobject_available(function() {
-	    swfobject.embedSWF(_jaycut.__loaderUri, 'jaycut-editor', '100%', '100%', '9.0.0', _jaycut.__loaderUri, flashvars, params);
+	    swfobject.embedSWF(_jaycut.__loaderUri, _jaycut.__options['embed_target'],
+			       _jaycut.__options['embed_width'], _jaycut.__options['embed_height'], '9.0.0',
+			       _jaycut.__loaderUri, _jaycut.__flashvars, _jaycut.__flashparams);
 	});
     }
 };
@@ -78,5 +117,45 @@ var __run_when_swfobject_available = function(func) {
 	setTimeout(function() { __run_when_swfobject_available(func) }, 50);
 }
 
+function isArray(obj) {
+    return obj.constructor == Array;
+}
+
+function isString(obj) {
+    return obj.constructor == String;
+}
+
+function build_chain_params(cp_hash) {
+    var result = ''
+    var parts = __bcps(cp_hash);
+    for (var i in parts) {
+	result += '&chain_params' + parts[i]
+    }
+    return result;
+}
+
+function __bcps(x) {
+    if (isString(x)) {
+	return ['=' + x]
+    } else if (isArray(x)) {
+	var res = new Array()
+	for (var elem in x) {
+	    var parts = __bcps(x[elem])
+	    for (var part in parts) {
+		res.push('[]' + parts[part]);
+	    }
+	}
+	return res;
+    } else {
+	var res = new Array()
+	for (var key in x) {
+	    var parts = __bcps(x[key])
+	    for (var part in parts) {
+		res.push('[' + key + ']' + parts[part]);
+	    }
+	}
+	return res;
+    }
+}
 
 var JC = _jaycut; // shorthand!
