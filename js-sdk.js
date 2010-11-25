9e7bc74b720854296d2a9cfc37a237aef6bd2811
Jerker Karlsson <jerker@jaycut.com>
2010-11-25 15:46:24 +0100

    whitespace - noop

--- js-sdk.js
+++ js-sdk.js
@@ -1,7 +1,7 @@
 function ensureSWFObjectScriptIsLoaded() {
     if (window.swfobject !== undefined) {
-	// Already loaded on the calling domain, don't include twice.
-	return;
+        // Already loaded on the calling domain, don't include twice.
+        return;
     }
     var head = document.getElementsByTagName("head")[0];
     script = document.createElement('script');
@@ -17,104 +17,104 @@ var _jaycut = {
     __loaderUri: null,
     __appUri:  null,
     __options: {
-	'version': 2,
-	'applet': 'login',
-	'chain': 'mixer',
-	'chain_params': {},
-	'embed_target': 'jaycut-editor',
-	'embed_width': '100%',
-	'embed_height': '100%'
+        'version': 2,
+        'applet': 'login',
+        'chain': 'mixer',
+        'chain_params': {},
+        'embed_target': 'jaycut-editor',
+        'embed_width': '100%',
+        'embed_height': '100%'
     },
     __flashvars: {},
     __flashparams: {
-	wmode: 'window',
-	allowScriptAccess: 'always',
-	allowFullScreen: 'true',
-	bgcolor: '#000000',
+        wmode: 'window',
+        allowScriptAccess: 'always',
+        allowFullScreen: 'true',
+        bgcolor: '#000000',
     },
     __flashoptions: {
-	'noHelp': true,
-	'noUpload': true,
-	'noPublish': true,
-	'noFullscreen': true,
-	'noOpenSave': true,
-	'noWebcam': true,
-	'noMicrophone': true,
-	'mix': true,
-	'remix': true,
-	'redirectOnPublish': true,
-	'redirectWindow': true,
-	'requireMixName': true,
-	'locale': true,
-	'helpUri': true
+        'noHelp': true,
+        'noUpload': true,
+        'noPublish': true,
+        'noFullscreen': true,
+        'noOpenSave': true,
+        'noWebcam': true,
+        'noMicrophone': true,
+        'mix': true,
+        'remix': true,
+        'redirectOnPublish': true,
+        'redirectWindow': true,
+        'requireMixName': true,
+        'locale': true,
+        'helpUri': true
     },
 
     subscribe: function(event_name, func) {
-	this.__event_handlers[event_name] = func;
+        this.__event_handlers[event_name] = func;
     },
     trigger: function(event_name, data) {
-	var func = this.__event_handlers[event_name];
-	if (func != null) {
-	    if (data)
-		return func(data);
-	    else
-		return func();
-	}
+        var func = this.__event_handlers[event_name];
+        if (func != null) {
+            if (data)
+                return func(data);
+            else
+                return func();
+        }
     },
     overrideLoaderUri: function(uri) {
-	this.__loaderUri = uri;
+        this.__loaderUri = uri;
     },
     overrideApplicationUri: function(uri) {
-	this.__appUri = uri;
+        this.__appUri = uri;
     },
     init: function(options) {
-	if (options != null) {
-	    for (var key in options) {
-		if (key == 'flashparams') {
-		    for (var k in options['flashparams']) {
-			this.__flashparams[k] = options['flashparams'][k]
-		    }
-		} else if (this.__flashoptions[key] == true) {
-		    this.__flashvars[key] = options[key];
-		} else {
-		    this.__options[key] = options[key]
-		}
-	    }
-	}
+        if (options != null) {
+            for (var key in options) {
+                if (key == 'flashparams') {
+                    for (var k in options['flashparams']) {
+                        this.__flashparams[k] = options['flashparams'][k]
+                    }
+                } else if (this.__flashoptions[key] == true) {
+                    this.__flashvars[key] = options[key];
+                } else {
+                    this.__options[key] = options[key]
+                }
+            }
+        }
 
-	if (this.__loaderUri == null) {
-	    this.__loaderUri = 'http://' + this.__options['uri_authority'] + '/assets/flash/ApplicationLoader.swf'
-	}
+        if (this.__loaderUri == null) {
+            this.__loaderUri = 'http://' + this.__options['uri_authority'] + '/assets/flash/ApplicationLoader.swf'
+        }
 
-	if (this.__appUri == null) {
-	    this.__appUri = 'http://' + this.__options['uri_authority'];
-	    this.__appUri += '/applets/' + this.__options['applet'] + '.xml';
-	    this.__appUri += '?chain=' + this.__options['chain'];
-	    this.__appUri += '&version=' + this.__options['version'];
-	    this.__appUri += build_chain_params(this.__options['chain_params']);
-	}
+        if (this.__appUri == null) {
+            this.__appUri = 'http://' + this.__options['uri_authority'];
+            this.__appUri += '/applets/' + this.__options['applet'] + '.xml';
+            this.__appUri += '?chain=' + this.__options['chain'];
+            this.__appUri += '&version=' + this.__options['version'];
+            this.__appUri += build_chain_params(this.__options['chain_params']);
+        }
 
-	this.__flashvars.applicationUri = encodeURIComponent(this.__appUri);
+        this.__flashvars.applicationUri = encodeURIComponent(this.__appUri);
 
-	if (this.__options['login_uri'] != null) {
-	    this.__flashvars.loginUri = encodeURIComponent(this.__options['login_uri']);
-	}
+        if (this.__options['login_uri'] != null) {
+            this.__flashvars.loginUri = encodeURIComponent(this.__options['login_uri']);
+        }
 
-	ensureSWFObjectScriptIsLoaded();
+        ensureSWFObjectScriptIsLoaded();
 
-	__run_when_swfobject_available(function() {
-	    swfobject.embedSWF(_jaycut.__loaderUri, _jaycut.__options['embed_target'],
-			       _jaycut.__options['embed_width'], _jaycut.__options['embed_height'], '9.0.0',
-			       _jaycut.__loaderUri, _jaycut.__flashvars, _jaycut.__flashparams);
-	});
+        __run_when_swfobject_available(function() {
+            swfobject.embedSWF(_jaycut.__loaderUri, _jaycut.__options['embed_target'],
+                               _jaycut.__options['embed_width'], _jaycut.__options['embed_height'], '9.0.0',
+                               _jaycut.__loaderUri, _jaycut.__flashvars, _jaycut.__flashparams);
+        });
     }
 };
 
 var __run_when_swfobject_available = function(func) {
     if (window.swfobject !== undefined)
-	func();
+        func();
     else
-	setTimeout(function() { __run_when_swfobject_available(func) }, 50);
+        setTimeout(function() { __run_when_swfobject_available(func) }, 50);
 }
 
 function isArray(obj) {
@@ -129,32 +129,32 @@ function build_chain_params(cp_hash) {
     var result = ''
     var parts = __bcps(cp_hash);
     for (var i in parts) {
-	result += '&chain_params' + parts[i]
+        result += '&chain_params' + parts[i]
     }
     return result;
 }
 
 function __bcps(x) {
     if (isString(x)) {
-	return ['=' + x]
+        return ['=' + x]
     } else if (isArray(x)) {
-	var res = new Array()
-	for (var elem in x) {
-	    var parts = __bcps(x[elem])
-	    for (var part in parts) {
-		res.push('[]' + parts[part]);
-	    }
-	}
-	return res;
+        var res = new Array()
+        for (var elem in x) {
+            var parts = __bcps(x[elem])
+            for (var part in parts) {
+                res.push('[]' + parts[part]);
+            }
+        }
+        return res;
     } else {
-	var res = new Array()
-	for (var key in x) {
-	    var parts = __bcps(x[key])
-	    for (var part in parts) {
-		res.push('[' + key + ']' + parts[part]);
-	    }
-	}
-	return res;
+        var res = new Array()
+        for (var key in x) {
+            var parts = __bcps(x[key])
+            for (var part in parts) {
+                res.push('[' + key + ']' + parts[part]);
+            }
+        }
+        return res;
     }
 }
 
