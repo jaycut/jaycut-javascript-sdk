fefab1d8bb855b367fb05d86cca8b2d976759247
jkarlsson <mail@jerkerkarlsson.se>
2011-03-01 09:00:11 -0800

    correct accidentally changed default value of embed_target parameter

--- js-sdk.js
+++ js-sdk.js
@@ -16,7 +16,7 @@ var _jaycut = {
     'loader': 'basicLoader',
     'chain': 'mixer',
     'chain_params': {},
-    'embed_target': 'jayCutEditor', // Target HTML element ID
+    'embed_target': 'jaycut-editor', // Target HTML element ID
     'embed_width': '100%',
     'embed_height': '100%',
     'uri_authority': '',
